78e7800a52eb49ed162e3f1269a8a3e9b2573382
rotex <dantejake48@gmail.com>
2023-01-19 04:46:21 +0100

    task fixx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,53 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
 include stdlib
+include stdlib
 
-$link = "/etc/nginx/sites-available/default"
-$custom_header = "add_header X-Served-By \$HOSTNAME;"
+$link = 'https://www.youtube.com/watch?v=PCfiqY05BpA'
+$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
+$custom_header = "add_header X-Served-By \$hostname;"
 
-exec { 'exec commands':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a $custom_header;" $link;
-  service nginx restart',
+exec { 'update packages':
+  command => 'sudo apt-get update'
   provider => shell,
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update packages']
+}
+
+
+exec { 'restart nginx':
+  command => 'sudo service nginx restart',
+  require => Package['nginx']
+  provider => shell,
+}
+
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root'
+}
+
+file_line { 'Set 301 redirection':
+  ensure   => 'present',
+  after    => 'server_name\ _;',
+  path     => '/etc/nginx/sites-available/default',
+  multiple => true,
+  line     => $content,
+  notify   => Exec['restart nginx'],
+  require  => File['/var/www/html/index.html']
+}
+
+file_line { 'Set X-Served-By header':
+  ensure   => 'present',
+  after    => 'http {',
+  path     => '/etc/nginx/nginx.conf',
+  multiple => true,
+  line     => $custom_header,
+  notify   => Exec['restart nginx'],
+  require  => File['/var/www/html/index.html']
 }
