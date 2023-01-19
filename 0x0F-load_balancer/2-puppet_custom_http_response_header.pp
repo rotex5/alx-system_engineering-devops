bfe4d6d976a7d1768baa3711d50822f5bd4ed521
rotex <dantejake48@gmail.com>
2023-01-19 11:58:25 +0100

    code refactor

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,27 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
+
 include stdlib
 
-$link = "/etc/nginx/sites-available/default"
-$custom_header = "add_header X-Served-By \$HOSTNAME;"
+$custom_header = "add_header X-Served-By \$hostname;"
 
-exec { 'exec commands':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a $custom_header;" $link;
-  service nginx restart',
+exec {'install':
   provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
+
+
+exec { 'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell
+}
+
+
+file_line { 'Insert X-Served-By header':
+  ensure   => 'present',
+  after    => 'http {',
+  path     => '/etc/nginx/nginx.conf',
+  multiple => true,
+  line     => $custom_header,
+  notify   => Exec['restart nginx'],
+  require  => File['/var/www/html/index.nginx-debian.html']
 }
