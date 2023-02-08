fdce7fa5225852cbe27735c7086306f257d815c7
rotex <dantejake48@gmail.com>
2023-02-08 19:05:15 +0100

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+and script to export data in the CSV format
+"""
+import requests
+from sys import argv
+
+
+def generate_csv(id):
+    """ export user data to csv"""
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = id
+    filename = "{}.csv".format(id)
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todos = requests.get(url + "users/{}/todos".format(user_id)).json()
+
+    with open(filename, "a") as f:
+        for task in todos:
+            f.write('"{}","{}","{}","{}"\n'.format(
+                task.get('userId'), user.get('name'),
+                task.get('completed'), task.get('title')))
+
+
+if __name__ == "__main__":
+    generate_csv(argv[1])
