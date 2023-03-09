f732bde60aa53614582ec42f4a0faacccefce92c
rotex <dantejake48@gmail.com>
2023-03-09 13:35:36 +0100

    readme and fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fix the number of max open files per process
+
+exec {'fix nginx and restart':
+command  => "sudo sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart",
+path     => ['/usr/bin', '/bin'],
+}
